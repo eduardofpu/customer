51bde1a05599a2a6d615fd09166c7102bd6e89b7
eduardo <eduardo27_minotauro@hotmail.com>
2018-09-05 17:41:54 -0300

    ajuste

--- src/main/java/br/com/zup/customer/domain/Customer.java
+++ src/main/java/br/com/zup/customer/domain/Customer.java
@@ -1,11 +1,10 @@
 package br.com.zup.customer.domain;
 
-import br.com.zup.customer.repository.CustomerRepository;
-import br.com.zup.customer.service.ValidatorService;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
 
@@ -26,32 +25,8 @@ public class Customer {
 
     @ManyToOne
     @JoinColumn(name="city")
+    @NaturalId
     private City city;
 
-    public Customer(Long id, String name, City city) {
-        this.id = id;
-        this.name = name;
-        this.city = city;
-    }
 
-    public Customer(String name, City city) {
-        this.name = name;
-        this.city = city;
-    }
-
-    public static Customer save(String name, City city, CustomerRepository customerRepository){
-
-       Customer customer = new Customer(name, city);
-        Customer save = customerRepository.save(customer);
-        return save;
-    }
-
-    public static Customer update(Long id, String name, City city, CustomerRepository customerRepository, ValidatorService validator){
-
-       validator.verifyIfUserExists(id);
-
-        Customer customer = new Customer(id, name, city);
-        Customer save = customerRepository.save(customer);
-        return save;
-    }
 }
--- src/main/java/br/com/zup/customer/service/ValidatorService.java
+++
@@ -1,5 +0,0 @@
-package br.com.zup.customer.service;
-
-public interface ValidatorService {
-    void verifyIfUserExists(Long id);
-}
